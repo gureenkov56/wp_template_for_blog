055643861d95e4a7fdcbd95881e20af9dee6d41e
gureenkov56 <gureenkov56@gmail.com>
2021-05-30 22:39:54 +1000

    Related posts - in process

--- single.php
+++ single.php
@@ -2,7 +2,8 @@
 
 <div class="content">
     <div class="content-container">
-        <?php if(has_post_thumbnail( $post_id )): //проверяем есть ли у поста миниатюра ?> 
+        <?php if (has_post_thumbnail($post_id)) : //проверяем есть ли у поста миниатюра 
+        ?>
             <div class="main-image" style="background-image: url(' <?php echo get_the_post_thumbnail_url(); ?> ');"></div>
         <?php endif ?>
 
@@ -11,6 +12,7 @@
 
             <?php the_content() ?>
         </div>
+        <div class="content-container__related-post">Relate posts</div>
     </div>
 </div>
 
