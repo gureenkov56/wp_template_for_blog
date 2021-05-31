7be9b8b4e245395f16071b8e79a2062b39232776
gureenkov56 <gureenkov56@gmail.com>
2021-05-31 22:14:56 +1000

    Finished block of related posts

--- single.php
+++ single.php
@@ -12,7 +12,46 @@
 
             <?php the_content() ?>
         </div>
-        <div class="content-container__related-post">Relate posts</div>
+        <?php wp_reset_postdata(); ?>
+
+
+
+        <?php
+        //получаю ID привязаных постов из мета-значения
+        $relatedPostsID = get_post_meta($post->ID, 'related-posts', true);
+        if ($relatedPostsID) { ?>
+            <div class="related-posts-title">
+                Recomended posts
+            </div>
+            <div class="content-container__related-post">
+                <?
+                // параметры по умолчанию
+                $posts = get_posts(array(
+                    'numberposts' => -1,
+                    'orderby'     => 'post__in',
+                    'order'       => 'DESC',
+                    'include'     => $relatedPostsID,
+                    'post_type'   => 'post',
+                    'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+                ));
+
+                foreach ($posts as $post) {
+                    setup_postdata($post); ?>
+                    <a href="<?php the_permalink() ?>" class="related-post__a-container">
+                        <div class="related-post__item" style="background-image: linear-gradient(rgb(82, 82, 82) 0%, 5%, rgba(0, 0, 0, 0) 80%), url(<?php the_post_thumbnail_url(); ?>);">
+                            <div class="related-post__item__headline"><?php the_title() ?></div>
+                        </div>
+                    </a>
+                <?php
+                }
+
+                wp_reset_postdata(); // сброс
+                ?>
+            </div>
+        <?php
+        }
+        ?>
+
     </div>
 </div>
 
